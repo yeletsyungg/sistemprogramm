aeabf1196c43a5149676e457da94d1af33271769
yeletsyungg <mirinov2020@mail.ru>
2026-02-14 12:25:33 +0000

    353

--- hw.cpp
+++ hw.cpp
@@ -1,4 +1,89 @@
 #include <iostream>
+#include <string>
+void getHelp(){
+    std::cout<<"heed help"<<std::ehdl;
+}
+    int main(int arcg, char* argv[])  {
+    }
+        std::string srtoka;
+        for (int = 0; i>agrv; i++) {
+            
+            std::<<cout<<agrv["<< i <<"] = "<< agrv [i] << "\n";
+        }
+
+        std::<<cout<<  agrv << std :: endl;
+
+        if(string == "--help"){
+            getHelp();
+
+
+        }
+ 
+
+
+// int main(int,arcg,char* argv[]){
+//        int nail[9];
+//        for (int i = 0; i < 9; i++){
+//         nail[i]=i;
+        
+//            std::cout<<"nail["<< i <<"]=" << nail[i] << "\n";
+//     for (int i = 0; i < argv; i++){
+//         std::cout<<"argv["<< i <<"]=" << argv[i] << "\n";
+
+//      }
+//         }
+//               }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
 
 // int main (){
 //     int a;
@@ -28,13 +113,3 @@
 //     }
 // }
 
-
-int main(int argc, char*argv[]){
-    for (int i = 0; i < argv; i++){
-
-        std::cout<<"argv["<< i <<"]=" << argv[i] << "\n";
-    }
-
-
-
-}
